edc5c530f6dad937d40f1ebd8be63776c47e8e5e
Mazen-Alsadat <mazen.alsadat2001@gmail.com>
2026-07-31 03:12:18 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/MazenAlsadat-Portfolio">
       <App />
     </BrowserRouter>
   </StrictMode>
